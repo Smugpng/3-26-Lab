f93e901dac7accfdcb828de105376c54a77f6d90
Smug <zikadika2@gmail.com>
2024-03-28 16:32:04 -0400

    Fixed Task 2

--- Assets/Scripts/Task2.cs
+++ Assets/Scripts/Task2.cs
@@ -3,38 +3,40 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
+using Unity.VisualScripting;
+using UnityEditor.VersionControl;
 using UnityEngine;
 using Random = System.Random;
 
 public class Task2 : MonoBehaviour
 {
-    public string[] smallernames = new string[] { };
 
+    public string[] smallernames = new string[15];
+
+    Random Random = new Random();
     private HashSet<string> names = new HashSet<string>
     {"Whitley", "Byron", "Livia", "Mateo", "Madilyn", "Adriel", "Ariyah", "Kellan", "Nalani", "Mack", "Marlowe", "Kristopher", 
      "Eloise", "Mohammad", "Khaleesi", "Chance", "Pearl", "Justice", "Alicia", "Jair", "Scout", "Onyx", "Gracelyn", "Atlas", "Julianna"};
-    // Start is called before the first frame update
+  
+
     Random random = new Random();
    
     void Start()
     {
-
-        for (int i = 0; i < 15; i++) 
-        {
-            string smallernames = names.ElementAt(random.Next(names.Count));
-            
-        }
-
-        foreach (string smallarray in smallernames)
-        {
-            Debug.Log(smallarray.ToString());
-        }
         
-        names.IntersectWith(smallernames);
-        foreach (string names in names)
+        int nameCount = 0;
+        List<String> namesList = new List<string>(names);
+        while (nameCount < smallernames.Length && namesList.Count > 0)
         {
-            Debug.Log(names.ToString());
+            int randomCount = random.Next(namesList.Count);
+            string randomName = namesList[randomCount];
+            smallernames[nameCount++] = randomName;
+            namesList.Remove(randomName);
         }
+        Debug.Log("Created the name array:" + string.Join(", ", smallernames)); 
+
+        names.IntersectWith(smallernames);
+        Debug.Log("The array has duplicate names: " + string.Join(", ", names));
     }
 
     // Update is called once per frame
