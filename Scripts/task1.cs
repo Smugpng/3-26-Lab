a360c3f4c4bb7d59c2228e4d3a9c872375215d4d
JMUCF <ju398578@ucf.edu>
2024-03-28 13:51:53 -0400

    Create task1.cs

---
+++ Scripts/task1.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class task1 : MonoBehaviour
+{
+    private List<string> names = new List<string>
+    {"Whitley", "Byron", "Livia", "Mateo", "Madilyn", "Adriel", "Ariyah", "Kellan", "Nalani", "Mack", "Marlowe", "Kristopher", "Eloise", "Mohammad", "Khaleesi", "Chance", "Pearl", "Justice", "Alicia", "Jair", "Scout", "Onyx", "Gracelyn", "Atlas", "Julianna"};
+    void Start()
+    {
+
+    }
+}
