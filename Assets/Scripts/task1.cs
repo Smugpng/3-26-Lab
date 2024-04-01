de3184c8e0bc108b616e1c9fc0a2a434e319f468
JMUCF <ju398578@ucf.edu>
2024-03-31 22:27:17 -0400

    Finished task1

--- Assets/Scripts/task1.cs
+++ Assets/Scripts/task1.cs
@@ -8,17 +8,18 @@ public class task1 : MonoBehaviour
     private List<string> names = new List<string>() {"Whitley", "Byron", "Livia", "Mateo", "Madilyn", "Adriel", "Ariyah", "Kellan", "Nalani", "Mack", "Marlowe", "Kristopher", "Eloise", "Mohammad", "Khaleesi", "Chance", "Pearl", "Justice", "Alicia", "Jair", "Scout", "Onyx", "Gracelyn", "Atlas", "Julianna"};
     private List<string> first5 = new List<string>();
     private Queue<string> loginQueue = new Queue<string>();
+    private int totalUsers;
 
     Random rnd = new Random();
 
     void Start()
     {
+        totalUsers = names.Count;
         for(int i = 0; i <= 4; i++) //gets 5 initial people to be in the queue & removes them from the list
         {
             int r = rnd.Next(names.Count);
             char let = (char)('a' + r);
             loginQueue.Enqueue(names[r] + " " + let);
-            Debug.Log(loginQueue.Dequeue());
             first5.Add(names[r] + " " + let);
             names.RemoveAt(r);
         }
@@ -27,12 +28,23 @@ public class task1 : MonoBehaviour
         StartCoroutine("queueOperation");
     }
 
-    private IEnumerator queueOperation() //Dequeue someone from loginqueue and enqueue new name from names list until names list is empty
+    private IEnumerator queueOperation() //Dequeues someone from loginqueue and enqueues new name from names list until names list is empty
     {
-        for(int i = 0; i <= names.Count; i++) //this could work maybe switch to a foreach items in names?
+        for(int i = 0; i < totalUsers-1; i++)
         {
-            Debug.Log("in queueOperation");
+            //Debug.Log("in queueOperation");
+            int r = rnd.Next(names.Count);
+            char let = (char)('a' + r);
+            Debug.Log(loginQueue.Dequeue() + ". is now inside the game.");
+            if(names.Count > 0)
+            {
+                loginQueue.Enqueue(names[r] + " " + let);
+                Debug.Log(names[r] + " " + let + ". is trying to login and added to the login queue.");
+                names.RemoveAt(r);
+            }
             yield return new WaitForSeconds(1f);
         }
+        Debug.Log(loginQueue.Dequeue() + ". is now inside the game.");
+        Debug.Log("All users have joined.");
     }
 }
